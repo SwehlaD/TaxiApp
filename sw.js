6203b27c4bbcd255f1d64a9672140353b0a6fa90
Dimas Swehla <dimasswehla2@gmail.com>
2026-07-02 13:53:17 -0400

    Polish mobile driver dashboard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-app-v16';
+const CACHE_NAME = 'taxi-app-v17';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', function(event) {

78be5ca14e8280a859a563f22aff8c0a81fbed2b
Dimas Swehla <dimasswehla2@gmail.com>
2026-07-02 14:07:30 -0400

    Improve ride status badge contrast

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-app-v20';
+const CACHE_NAME = 'taxi-app-v21';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', function(event) {
